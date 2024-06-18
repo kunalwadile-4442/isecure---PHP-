06c8ef3d307dd7ae46f8da36abe1058be4cdd147
Gayatri <gayatrisadhav2002@gmail.com>
2024-06-18 18:40:12 +0530

    Isecure done

---
+++ partials/_nav.php
@@ -0,0 +1,44 @@
+<?php 
+
+if(isset($_SESSION['loggedin']) || $_SESSION['loggedin']==true){
+  $loggedin = true;
+}
+
+echo '
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="/LoginSystem">iSecure</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/LoginSystem/welcome.php">Home <span class="sr-only">(current)</span></a>
+      </li>';
+
+    if(!$loggedin){
+    echo ' <li class="nav-item">
+  <a class="btn btn-primary" href="/LoginSystem/login.php">Login</a>
+</li>
+<li class="nav-item mx-2">
+  <a class="btn btn-success" href="/LoginSystem/signup.php">Signup</a>
+</li>';
+}
+
+      if($loggedin){
+     echo' 
+     <li class="nav-item">
+        <a class="btn btn-danger" href="/LoginSystem/logout.php">logout</a>
+      </li>';
+      }
+ 
+
+   echo' </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>';
+
+?>
